dc4419971f6c4428466f3d7da299272f6898ab79
muriloSanches1001 <murilooliveirasanches1001@gmail.com>
2025-11-22 16:48:59 -0300

    style: apply eslint/stylelint autocorrect to the application shell

--- app.vue
+++ app.vue
@@ -1,14 +1,17 @@
+<script setup lang="ts">
+import BaseScrollWrapper from '~/components/ui/scroll/BaseScrollWrapper.vue'
+import { useAppScroll } from '~/composables/ui/app/useAppScroll'
+
+const { updateScrollTop } = useAppScroll()
+</script>
+
 <template>
-  <base-scroll-wrapper class="h-screen" @scroll="updateScrollTop">
+  <base-scroll-wrapper
+    class="h-screen"
+    @scroll="updateScrollTop"
+  >
     <nuxt-layout>
       <nuxt-page />
     </nuxt-layout>
   </base-scroll-wrapper>
 </template>
-
-<script setup lang="ts">
-import BaseScrollWrapper from "~/components/ui/scroll/BaseScrollWrapper.vue";
-import {useAppScroll} from "~/composables/ui/app/useAppScroll";
-
-const { updateScrollTop } = useAppScroll()
-</script>
