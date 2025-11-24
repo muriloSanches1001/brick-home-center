224cf2e39dee4a31875f5f4b03459e88c10e9e83
muriloSanches1001 <murilooliveirasanches1001@gmail.com>
2025-11-24 19:24:31 -0300

    feat(contact): add ContactThree section component and create contact page

---
+++ pages/contato/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import BasePageHeader from "~/components/ui/navigation/BasePageHeader.vue";
+import BaseSection from "~/components/ui/sections/BaseSection.vue";
+import TheMarquee from "~/components/layout/TheMarquee.vue";
+import ContactThree from "~/components/contact/ContactThree.vue";
+</script>
+
+<template>
+  <base-page-header />
+
+  <base-section padding-y="lg">
+    <contact-three />
+  </base-section>
+
+  <the-marquee />
+</template>
+
+<style scoped>
+
+</style>
