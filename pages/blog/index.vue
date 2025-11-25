90293e96a936ade2b84bf36ab70d62ccf631a32e
muriloSanches1001 <murilooliveirasanches1001@gmail.com>
2025-11-24 21:55:56 -0300

    feat(blog): create blog listing page and BlogCatalog component

---
+++ pages/blog/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import BaseSection from '~/components/ui/sections/BaseSection.vue'
+import TheMarquee from '~/components/layout/TheMarquee.vue'
+import BasePageHeader from '~/components/ui/navigation/BasePageHeader.vue'
+import BlogCatalog from '~/components/blog/BlogCatalog.vue'
+</script>
+
+<template>
+  <div>
+    <base-page-header />
+
+    <base-section padding-y="lg">
+      <blog-catalog />
+    </base-section>
+
+    <the-marquee />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
